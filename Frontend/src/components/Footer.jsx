8c93f95e7e20ebf8afa972aac057a42f2db53c87
Bulbul87 <vishwakarmabulbul87@gmail.com>
2025-05-12 12:01:19 +0530

    Footer second update text

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <footer className="bg-violet-800 text-white text-center p-4 font-semibold mt-auto">
       <p className="text-sm md:text-base">
-        © {new Date().getFullYear()} Your Company. All rights reserved at new years.
+        © {new Date().getFullYear()} Your Company. All rights reserved .
       </p>
       <p className="text-sm md:text-base">
-        Designed by <span className="font-bold">peter</span>
+        Designed by <span className="font-bold">Makvin</span>
       </p>
     </footer>
   );
